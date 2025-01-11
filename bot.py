e25a60ddc8f08db28985c1a9404d23c9436cb8d7
lremor <luisremor@hotmail.com>
2025-01-11 15:00:43 -0300

    start

---
+++ bot.py
@@ -0,0 +1,23 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+from constants import TOKEN
+
+load_dotenv()
+
+intents = discord.Intents.default()
+
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+ #LOLOLLOL
+
+client.run(TOKEN)
