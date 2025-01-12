29538aa161aaf19f96f92a7c4d613f498c7a735f
lremor <luisremor@hotmail.com>
2025-01-11 22:47:52 -0300

    update security

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 from discord.ext.commands import Context
 import TrackerGG
 from TrackerGG import CSGOClient
-from constants import EMOJIS, TOKEN, TRACKERAPI_KEY
+from constants import EMOJIS, TOKEN, TRACKERAPI_KEY, ID
 
 intents = discord.Intents.default()
 
@@ -19,14 +19,13 @@ bot: commands.Bot = commands.Bot(
 )
 
 client = CSGOClient(TRACKERAPI_KEY)
-id = 1324715914180169733
 
 def get_error_embed(desc: str) -> discord.Embed:
     return discord.Embed(title=":no_entry: Error", description=desc, color=0xFF0000)
 
 @bot.event
 async def on_ready():
-    channelID = bot.get_channel(id)
+    channelID = bot.get_channel(ID)
     game = discord.Game(f"Counter-Strike")
     await bot.change_presence(status=discord.Status.online, activity=game)
     await channelID.send(f'TO ON')
@@ -34,18 +33,18 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    channelID = bot.get_channel(id)
+    channelID = bot.get_channel(ID)
     await channelID.send(f'EAE {member.mention}!')
     await channelID.edit(name = 'DISCORD LIVRE DE LTX! Membros: {}'.format(channelID.guild.member_count))
     print('Contagem concluída')
     
 
 @bot.command()
-async def msg(ctx, channel_id: int, *, mensagem: str):
-    channel = bot.get_channel(channel_id)
-    if channel:
-        await channel.send(mensagem)
-        print(f'Mensagem enviada para o canal {channel.name}')
+async def msg(ctx, mensagem: str):
+    channelID = bot.get_channel(ID)
+    if channelID:
+        await channelID.send(mensagem)
+        print(f'Mensagem enviada para o canal {channelID.name}')
     else:
         print('Canal não encontrado.')
 
