15a34bf4952e9355f500684f3bcf15900dc70c87
lremor <luisremor@hotmail.com>
2025-01-11 23:01:04 -0300

    correcoes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 from discord.ext.commands import Context
 import TrackerGG
 from TrackerGG import CSGOClient
-from constants import EMOJIS, TOKEN, TRACKERAPI_KEY, ID
+from constants import EMOJIS, TOKEN, ID_SERVER, ID_CHANNEL
 
 intents = discord.Intents.default()
 
@@ -18,30 +18,32 @@ bot: commands.Bot = commands.Bot(
     command_prefix="!", intents=intents, case_insensitive=True
 )
 
-client = CSGOClient(TRACKERAPI_KEY)
 
 def get_error_embed(desc: str) -> discord.Embed:
     return discord.Embed(title=":no_entry: Error", description=desc, color=0xFF0000)
 
 @bot.event
 async def on_ready():
-    channelID = bot.get_channel(ID)
+    channelID = bot.get_channel(ID_CHANNEL)
     game = discord.Game(f"Counter-Strike")
     await bot.change_presence(status=discord.Status.online, activity=game)
     await channelID.send(f'TO ON')
+
     print(f'{bot.user} conectou no Discord!')
 
 @bot.event
 async def on_member_join(member):
-    channelID = bot.get_channel(ID)
-    await channelID.send(f'EAE {member.mention}!')
-    await channelID.edit(name = 'DISCORD LIVRE DE LTX! Membros: {}'.format(channelID.guild.member_count))
+    guild = bot.get_guild(ID_SERVER)
+    member_count = guild.member_count
+    channelID = bot.get_channel(ID_CHANNEL)
+    await channelID.send(f'EAE {member}!')
+    await channelID.edit(name = f'DISCORD LIVRE DE LTX! Membros: {member_count} ')
     print('Contagem concluída')
     
 
 @bot.command()
 async def msg(ctx, mensagem: str):
-    channelID = bot.get_channel(ID)
+    channelID = bot.get_channel(ID_CHANNEL)
     if channelID:
         await channelID.send(mensagem)
         print(f'Mensagem enviada para o canal {channelID.name}')
