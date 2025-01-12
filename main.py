13f647ca9baefa438b418b2dd38d1f68f531c3c8
lremor <luisremor@hotmail.com>
2025-01-12 17:48:43 -0300

    comando !uptime

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import discord
 from discord.ext import commands
 from discord.ext.commands import Context
@@ -8,6 +9,9 @@ load_dotenv()
 TOKEN = os.getenv('TOKEN')
 ID_CHANNEL = int(os.getenv('ID_CHANNEL'))
 ID_SERVER = int(os.getenv('ID_SERVER'))
+ID_LTX = int(os.getenv('ID_LTX'))
+
+start_time = None
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -21,24 +25,48 @@ bot: commands.Bot = commands.Bot(
 def get_error_embed(desc: str) -> discord.Embed:
     return discord.Embed(title=":no_entry: Error", description=desc, color=0xFF0000)
 
+
 @bot.event
 async def on_ready():
+    global start_time
+    start_time = datetime.datetime.now()
     channelID = bot.get_channel(ID_CHANNEL)
     game = discord.Game(f"Counter-Strike")
     await bot.change_presence(status=discord.Status.online, activity=game)
     await channelID.send(f'TO ON')
-
     print(f'{bot.user} conectou no Discord!')
+
 
 @bot.event
 async def on_member_join(member):
     guild = bot.get_guild(ID_SERVER)
     member_count = guild.member_count
     channelID = bot.get_channel(ID_CHANNEL)
+    if member.id == ID_LTX:
+        try:
+            await member.ban(reason="Galera não te curte.")
+            print(f'LTX banido.')
+        except Exception as e:
+            print(f'Erro ao banir o membro: {e}')
     await channelID.send(f'EAE {member}!')
-    await channelID.edit(subject = f'DISCORD LIVRE DE LTX! Membros: {member_count} ')
-    print('Contagem concluída')
-    
+    await channelID.edit(subject = f'DISCORD LIVRE DE LTX! Membros: {member_count}')
+    print(f'Assunto editado para: DISCORD LIVRE DE LTX! Membros: {member_count} ')
+
+@bot.command()
+async def uptime(ctx):
+    channelID = bot.get_channel(ID_CHANNEL)
+    if start_time:
+        current_time = datetime.datetime.now()
+        uptime_duration = current_time - start_time
+        hours, remainder = divmod(uptime_duration.total_seconds(), 3600)
+        minutes, seconds = divmod(remainder, 60)
+        upmsg = f'O bot está online há {int(hours)} horas, {int(minutes)} minutos e {int(seconds)} segundos.'
+        await channelID.send(upmsg)
+        print(upmsg)
+    else:
+        noupmsg = 'O tempo de início não foi registrado.'
+        await channelID.send(noupmsg)
+        print(noupmsg)
 
 @bot.command()
 async def msg(ctx, mensagem: str):
