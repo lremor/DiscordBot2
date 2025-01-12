4a9b289f4e8f0191ba5606d55da1062aeca9524c
lremor <luisremor@hotmail.com>
2025-01-12 18:18:10 -0300

    avisar no console quem conectou

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ start_time = None
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.guilds=True
+intents.presences=True
 client = discord.Client(intents=intents)
 
 bot: commands.Bot = commands.Bot(
@@ -24,7 +26,6 @@ bot: commands.Bot = commands.Bot(
 
 def get_error_embed(desc: str) -> discord.Embed:
     return discord.Embed(title=":no_entry: Error", description=desc, color=0xFF0000)
-
 
 @bot.event
 async def on_ready():
@@ -36,6 +37,11 @@ async def on_ready():
     await channelID.send(f'TO ON')
     print(f'{bot.user} conectou no Discord!')
 
+@bot.event
+async def on_message(message):
+    if bot.user.mentioned_in(message):
+        await message.channel.send(f'Fala tu {message.author.mention}, estou proibido de falar! by lelo')
+    await bot.process_commands(message)
 
 @bot.event
 async def on_member_join(member):
@@ -51,6 +57,11 @@ async def on_member_join(member):
     await channelID.send(f'EAE {member}!')
     await channelID.edit(subject = f'DISCORD LIVRE DE LTX! Membros: {member_count}')
     print(f'Assunto editado para: DISCORD LIVRE DE LTX! Membros: {member_count} ')
+
+@bot.event
+async def on_presence_update(before, after):
+    if before.status != after.status and after.status == discord.Status.online:
+        print(f'{after.name} acabou de ficar online!')
 
 @bot.command()
 async def uptime(ctx):
